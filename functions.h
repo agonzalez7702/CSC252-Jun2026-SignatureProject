966289822a7bcd7b3355a3dca4c86be54cf6c9e5
Greg Corder <g.corder8311@student.nu.edu>
2026-06-19 09:51:28 -0400

    Add selection sort and binary search functions
    Added sorting and searching functions to the header.

--- functions.h
+++ functions.h
@@ -6,13 +6,15 @@
 // NEVER use "using namespace std;" in header files
 
 // Declaring constants
-const int SIZE = 15;
+// const int SIZE = 15; //Greg doesn't think this is needed but doesn't want to delete it yet in case something breaks
 
 // Declaring functions
 
 int* createArray(int); // This is the function call to generate the numbers
 void writeBinary(std::string, int*, int);
 void createBinaryFile(std::string filename);
-
+void selection_sort(int* values, int size); //Greg added other functions required here and below
+bool binary_search_recursive(int* values, int key, int start, int end);
+bool binary_search(int* values, int key, int size);
 
 
